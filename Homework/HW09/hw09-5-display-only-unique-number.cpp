8c7958c69d348b1d2f9425da655bf0cc75625d6d
tatar008 <jirapat_ja66@live.rmutl.ac.th>
2023-09-20 20:08:04 +0700

    hw09-5

--- Homework/HW09/hw09-5-display-only-unique-number.cpp
+++ Homework/HW09/hw09-5-display-only-unique-number.cpp
@@ -37,3 +37,53 @@
         Unique value : 1 2 5 6
 
 */
+#include <stdio.h>
+
+int main() {
+    int N;
+    int i ;
+    int arr[N] ;
+    printf("Input N: ");
+    scanf("%d", &N);
+    
+    for ( i = 0; i < N; i++) {
+        printf("Input: ");
+        scanf("%d", &arr[i]);
+    }
+    
+    int unique[N];
+    int uniqueCount = 0;
+    
+    for (int i = 0; i < N; i++) {
+        int isUnique = 1;
+        
+        for (int j = 0; j < uniqueCount; j++) {
+            if (arr[i] == unique[j]) {
+                isUnique = 0;
+                break;
+            }
+        }
+        
+        if (isUnique) {
+            unique[uniqueCount] = arr[i];
+            uniqueCount++;
+        }
+    }
+    
+    for (int i = 0; i < uniqueCount - 1; i++) {
+        for (int j = 0; j < uniqueCount - i - 1; j++) {
+            if (unique[j] > unique[j + 1]) {
+                int temp = unique[j];
+                unique[j] = unique[j + 1];
+                unique[j + 1] = temp;
+            }
+        }
+    }
+    
+    printf("Unique value : ");
+    for (int i = 0; i < uniqueCount; i++) {
+        printf("%d ", unique[i]);
+    }
+    
+    return 0;
+}
